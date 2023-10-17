83bab5b00b15dd162dd30089a4da88a8f2fd2fd6
SamPro07 <samirkhot67@gmail.com>
2023-10-17 16:04:58 +0100

    created skeleton script

---
+++ delete-repo.py
@@ -0,0 +1,39 @@
+import requests
+
+# Replace with your GitHub username and access token
+username = 'your_username'
+access_token = 'your_access_token'
+
+# Function to delete a GitHub repository
+def delete_repository(repo_name):
+    url = f'https://api.github.com/repos/{username}/{repo_name}'
+    headers = {'Authorization': f'token {access_token}'}
+
+    try:
+        response = requests.delete(url, headers=headers)
+
+        if response.status_code == 204:
+            print(f"Repository {repo_name} deleted successfully.")
+        elif response.status_code == 404:
+            print(f"Repository {repo_name} not found.")
+        else:
+            print(f"Failed to delete repository {repo_name}. Status code: {response.status_code}")
+            print(f"Response: {response.text}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+# List of repositories to be deleted
+repositories_to_delete = ['repo1', 'repo2', 'repo3']  # Add your repository names here
+
+print("The following repositories are scheduled for deletion:")
+for repo in repositories_to_delete:
+    print(repo)
+
+confirmation = input("Do you want to proceed with deletion? (yes/no): ")
+
+if confirmation.lower() == "yes":
+    for repo in repositories_to_delete:
+        print(f"Deleting {repo}...")
+        delete_repository(repo)
+else:
+    print("Deletion aborted. No repositories were deleted.")
